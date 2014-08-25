597526c4f5022d6bc85171107ffcd7a5751e60aa
chase <chase@bryx.com>
2014-08-25 10:32:14 -0400

    First work on these two classes. Looking into common functions for TBSUnit

---
+++ src/TBSMap.java
@@ -0,0 +1,12 @@
+/**
+ * Created by chase on 8/25/14.
+ */
+public class TBSMap {
+
+    private TBSTile[] mapTiles;
+
+
+
+
+
+}
---
+++ src/TBSUnit.java
@@ -0,0 +1,13 @@
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by chase on 8/25/14.
+ */
+public abstract class TBSUnit {
+
+    //TBSPlayer owner;s
+    BufferedImage defaultImage;
+
+    
+
+}
