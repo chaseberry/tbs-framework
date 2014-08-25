4113cd23fc07b3588319bd853f5b8e152241acf5
chase <chase@bryx.com>
2014-08-25 10:25:04 -0400

    Made abstract. Added onSelect and onDeselect functions

--- src/TBSTile.java
+++ src/TBSTile.java
@@ -3,20 +3,25 @@ import java.awt.image.BufferedImage;
 /**
  * Created by chase on 8/25/14.
  */
-public class TBSTile {
+public abstract class TBSTile {
 
     private int xLocation, yLocation;
     private BufferedImage defaultImage;
 
-    public TBSTile(int x, int y){
+    public TBSTile(int x, int y, BufferedImage tileImage){
         xLocation = x;
         yLocation = y;
+        defaultImage = tileImage;
     }
 
     public BufferedImage getImage(){
         return defaultImage;
     }
 
+    public abstract void onSelect();//will have params once those classes are defineds
 
+    public abstract void onDeselect();
+
+    
 
 }
